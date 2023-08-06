be5a5ce8cc99125b402b9fe4a6ae7d777f4cee1a
ｒｙｍｅ <ryme.care@hotmail.com>
2023-08-06 15:11:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, request
-from pymongo import MongoClient, errors
+from flask import Flask, request, jsonify, render_template
+from pymongo import MongoClient
+from pymongo.errors import DuplicateKeyError
 from datetime import datetime, timedelta
-from random import choice
-from string import ascii_uppercase, digits
-from bson.objectid import ObjectId
+import random
+import string
 import os
 
 app = Flask(__name__)
@@ -13,37 +13,54 @@ app = Flask(__name__)
 MONGODB_URL = os.getenv('MONGODB_URL', 'mongodb+srv://gen:gen@gen.fuemgjr.mongodb.net/?retryWrites=true&w=majority')
 
 client = MongoClient(MONGODB_URL)
-db = client["license_db"]
-codes = db["codes"]
-access_keys = db.get_collection('access_keys')
+db = client.licenceCodesDB
+codes = db.codes
+access_keys = db.access_keys
 
 def generate_license_code():
-    return ''.join(choice(ascii_uppercase + digits) for _ in range(10))
+    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
 
-@app.route('/')
+def generate_access_key():
+    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
+
+@app.route('/generate_access_key', methods=['GET'])
+def generate():
+    while True:
+        access_key = generate_access_key()
+        try:
+            access_keys.insert_one({"_id": access_key})
+            return {"access_key": access_key}, 200
+        except DuplicateKeyError:
+            continue
+
+@app.route('/', methods=['GET'])
+def landing_page():
+    return render_template('index.html')
+
+@app.route('/', methods=['GET'])
 def home():
     access_key = request.args.get('access_key')
-    user = access_keys.find_one({"_id": access_key})
-
-    if user is None:
+    operation = request.args.get('action')
+    validate = request.args.get('days', default=30)
+    code = request.args.get('code')
+    
+    # Validate access_key here
+    access_key_entry = access_keys.find_one({"_id": access_key})
+    if access_key_entry is None:
         return {"message": "Invalid access key"}, 403
 
-    action = request.args.get('action')
-    if action == 'generate':
-        time_in_days = int(request.args.get('days', 30))
-        expiration_date = datetime.now() + timedelta(days=time_in_days)
-
-        try:
-            license_code = generate_license_code() + str(user["_id"])
-            codes.insert_one({"_id": license_code, "expiration_date": expiration_date, "used": False, "user_id": str(user["_id"])})
-            return {"license_code": license_code}, 200
-        except errors.DuplicateKeyError:
-            return home()
-    elif action == 'validate':
-        code = request.args.get('code')
+    if operation == 'generate':
+        expiration_date = datetime.now() + timedelta(days=int(validate))
+        while True:
+            license_code = generate_license_code()
+            try:
+                codes.insert_one({"_id": license_code, "expiration_date": expiration_date, "used": False})
+                return {"license_code": license_code}, 200
+            except DuplicateKeyError:
+                continue
+    elif operation == 'validate':
         entry = codes.find_one({"_id": code})
-
-        if entry is None or entry["user_id"] != str(user["_id"]):
+        if entry is None:
             return {"message": "Invalid code"}, 404
         elif entry["used"] == True:
             return {"message": "This code is already in use"}, 403
@@ -52,28 +69,6 @@ def home():
         else:
             codes.update_one({"_id": code}, {"$set": {"used": True}})
             return {"message": "Code validated successfully"}, 200
-    else:
-        return render_template('index.html')
-
-@app.route('/generate_access_key')
-def generate_access_key():
-    user_id = ObjectId()
-    access_key = generate_license_code()
-
-    access_keys.insert_one({"_id": access_key, "user_id": str(user_id)})
-
-    return {"access_key": access_key}, 200
-
-
-
-# Add a handler for all uncaught exceptions
-@app.errorhandler(Exception)
-def handle_error(e):
-    code = 500
-    if isinstance(e, HTTPException):
-        code = e.code
-    return jsonify(error=str(e)), code
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
-
